233f66d1d8df60e8506346d3192d08a22f5e935e
Ruiyotabby <ruiyotabby@gmail.com>
2022-10-12 17:48:29 +0300

    Add a recursive method which splits the unsorted array to work array

--- merge_sort.rb
+++ merge_sort.rb
@@ -2,3 +2,14 @@
 def merge_sort(arr_a, arr_b, length)
   arr_a.each { |item| arr_b << item }
 end
+
+# Split arr_a into 2 runs, sort both runs into arr_b, merge both runs from B[] to A[]
+# iBegin is inclusive; iEnd is exclusive (A[iEnd] is not in the set).
+def split_merge(arr_b, first, last, arr_a)
+  return if last - first <= 1
+
+  middle = (first + last) / 2
+  split_merge(arr_a, first, middle, arr_b)
+  split_merge(arr_a, middle, last, arr_b)
+
+end
