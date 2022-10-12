569a875ee678ccf6b5c615bc0db4f789309b9795
Ruiyotabby <ruiyotabby@gmail.com>
2022-10-12 17:49:32 +0300

    Add a method which merges the arrays

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,6 +1,7 @@
 # Array arr_a has the items to sort; array arr_b is a work array.
 def merge_sort(arr_a, arr_b, length)
   arr_a.each { |item| arr_b << item }
+  split_merge(arr_b, 0, length, arr_a)
 end
 
 # Split arr_a into 2 runs, sort both runs into arr_b, merge both runs from B[] to A[]
@@ -12,4 +13,26 @@ def split_merge(arr_b, first, last, arr_a)
   split_merge(arr_a, first, middle, arr_b)
   split_merge(arr_a, middle, last, arr_b)
 
+  merge(arr_b, first, middle, last, arr_a)
 end
+
+def merge(arr_a, first, middle, last, arr_b)
+  i = first; j = middle
+
+  (first...last).each do |k|
+    if i < middle && (j >= last || arr_a[i] <= arr_a[j])
+      arr_b[k] = arr_a[i]
+      i += 1
+    else
+      arr_b[k] = arr_a[j]
+      j += 1
+    end
+  end
+end
+
+a = [2,3,4,2,8,6,9,3,5,22,66,33,78,4,325,789]
+b = []
+
+merge_sort(a, b, a.length)
+
+p b
