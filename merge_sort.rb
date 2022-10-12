b4dde6becc456e6493b9404b4f4bff9005f64a13
Ruiyotabby <ruiyotabby@gmail.com>
2022-10-12 17:50:43 +0300

    Add comments

--- merge_sort.rb
+++ merge_sort.rb
@@ -4,8 +4,8 @@ def merge_sort(arr_a, arr_b, length)
   split_merge(arr_b, 0, length, arr_a)
 end
 
-# Split arr_a into 2 runs, sort both runs into arr_b, merge both runs from B[] to A[]
-# iBegin is inclusive; iEnd is exclusive (A[iEnd] is not in the set).
+# Split arr_a into 2 runs, sort both runs into arr_b, merge both runs from arr_b to arr_a
+# first is inclusive; last is exclusive (arr_a[last] is not in the set).
 def split_merge(arr_b, first, last, arr_a)
   return if last - first <= 1
 
