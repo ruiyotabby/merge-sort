e2144435f5388c2c679de9f2b49dd1445962bbb2
Ruiyotabby <ruiyotabby@gmail.com>
2022-10-13 10:04:05 +0300

    Refactor code

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,38 +1,36 @@
-# Array arr_a has the items to sort; array arr_b is a work array.
-def merge_sort(arr_a, arr_b, length)
+def merge_sort(arr_a, arr_b)
   arr_a.each { |item| arr_b << item }
-  split_merge(arr_b, 0, length, arr_a)
+
+  split(arr_b, 0, arr_a.length, arr_a)
 end
 
-# Split arr_a into 2 runs, sort both runs into arr_b, merge both runs from arr_b to arr_a
-# first is inclusive; last is exclusive (arr_a[last] is not in the set).
-def split_merge(arr_b, first, last, arr_a)
+def split(arr_b, first, last, arr_a)
   return if last - first <= 1
 
-  middle = (first + last) / 2
-  split_merge(arr_a, first, middle, arr_b)
-  split_merge(arr_a, middle, last, arr_b)
+  mid = (last + first) / 2
 
-  merge(arr_b, first, middle, last, arr_a)
+  split(arr_a, first, mid, arr_b)
+  split(arr_a, mid, last, arr_b)
+
+  merge(arr_a, first, mid, last, arr_b)
 end
 
-def merge(arr_a, first, middle, last, arr_b)
-  i = first; j = middle
-
+def merge(arr_a, first, mid, last, arr_b)
+  i = first; j = mid
   (first...last).each do |k|
-    if i < middle && (j >= last || arr_a[i] <= arr_a[j])
-      arr_b[k] = arr_a[i]
+    if i < mid && (j >= last || arr_b[i] <= arr_b[j])
+      arr_a[k] = arr_b[i]
       i += 1
     else
-      arr_b[k] = arr_a[j]
+      arr_a[k] = arr_b[j]
       j += 1
     end
   end
 end
 
-a = [2,3,4,2,8,6,9,3,5,22,66,33,78,4,325,789]
+a = [3, 4, 6, 8, 4, 55, 677, 564, 344, 89, 4, 78]
 b = []
 
-merge_sort(a, b, a.length)
-
+p merge_sort(a, b)
+p a
 p b
